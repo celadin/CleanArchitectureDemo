7ff04c8a9560ca912e809b59e4b48025bd03262c
Celalettin Altıntaş <CELALETTIN ALTINTA>
2020-08-29 16:45:26 +0300

    UniversityDb Configured

--- CleanArchitectureDemo/CleanArchitecture.Mvc/Startup.cs
+++ CleanArchitectureDemo/CleanArchitecture.Mvc/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using CleanArchitecture.Infra.Data.Context;
 using CleanArchitecture.Mvc.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +27,14 @@ namespace CleanArchitecture.Mvc
                     Configuration.GetConnectionString("UniversityIdentityDbConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            services.AddDbContext<UniversityDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityDbConnection"));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -50,6 +53,7 @@ namespace CleanArchitecture.Mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -61,8 +65,8 @@ namespace CleanArchitecture.Mvc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
