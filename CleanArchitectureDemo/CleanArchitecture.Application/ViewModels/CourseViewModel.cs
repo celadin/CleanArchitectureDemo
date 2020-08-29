8900f4fdd31e3a35cfd7ad121cd3dee794f236fe
Celalettin Altıntaş <CELALETTIN ALTINTA>
2020-08-29 17:38:00 +0300

    Commit Service and Repostiroy Interfaces, ViewModels

---
+++ CleanArchitectureDemo/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CleanArchitecture.Domain.Models;
+
+namespace CleanArchitecture.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
