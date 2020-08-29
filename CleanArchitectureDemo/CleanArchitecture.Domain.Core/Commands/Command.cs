1047c2c8df4c436cc74e4c926a4903be0040f211
Celalettin Altıntaş <CELALETTIN ALTINTA>
2020-08-30 01:29:37 +0300

    Adding Domain.Core and implementation of InMemoryBus

---
+++ CleanArchitectureDemo/CleanArchitecture.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CleanArchitecture.Domain.Core.Events;
+
+namespace CleanArchitecture.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; protected set; }
+    }
+}
---
+++ CleanArchitectureDemo/CleanArchitecture.Domain.Core/Events/Message.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        protected Message()
+        {
+            Name = GetType().Name;
+        }
+
+        public string Name { get; protected set; }
+    }
+}
