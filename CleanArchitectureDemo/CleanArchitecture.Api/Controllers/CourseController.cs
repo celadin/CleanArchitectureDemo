942dedd141ce9a0b5f1d56b2c218a7469ce43089
Celalettin Altıntaş <CELALETTIN ALTINTA>
2020-08-30 03:06:08 +0300

    Added MediaTR to API Project, Created Course Api Method

--- CleanArchitectureDemo/CleanArchitecture.Api/Controllers/CourseController.cs
+++ CleanArchitectureDemo/CleanArchitecture.Api/Controllers/CourseController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Api.Controllers
 {
@@ -11,6 +8,18 @@ namespace CleanArchitecture.Api.Controllers
     [Route("[controller]")]
     public class CourseController : ControllerBase
     {
-        
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateCourseViewModel model)
+        {
+            _courseService.CreateCourse(model);
+            return Ok();
+        }
     }
 }
--- CleanArchitectureDemo/CleanArchitecture.Api/Startup.cs
+++ CleanArchitectureDemo/CleanArchitecture.Api/Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchitecture.Infra.Data.Context;
+using CleanArchitecture.Infra.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Api
 {
@@ -25,16 +22,21 @@ namespace CleanArchitecture.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UniversityDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityDbConnection"));
+            });
+
             services.AddControllers();
+
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
@@ -42,10 +44,12 @@ namespace CleanArchitecture.Api
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
